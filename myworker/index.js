de7c6d14e86c2200f6632f59b222a62c541a2e86
chappyhome <104469556@qq.com>
2024-10-10 14:55:06 +0800

    modify to myworker

--- myworker/index.js
+++ myworker/index.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request) {
       const quotes = [
         "Life is what happens when you're busy making other plans.",
-        "The purpose of our lives is to be happy.11111",
+        "The purpose of our lives is to be happy.111111",
         "Get busy living or get busy dying5555.",
         "You have within you right now, everything you need to deal with whatever the world can throw at you."
       ];
